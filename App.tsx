40be9945be929aabacc556e97c5bb9241de858cb
Pablo <pablohanry9@gmail.com>
2021-10-20 16:32:56 +0200

    feat: update stack

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeBaseProvider } from 'native-base';
+import theme from './theme/theme';
+import HomePage from './src/components/HomePage';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NativeBaseProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomePage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
+  );
+}
+
+export default App;
